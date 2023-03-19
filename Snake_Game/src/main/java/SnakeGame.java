f314b234446266dadabdba405149a0a1aab1f107
AmreshTripathy <amreshtripathy184@gmail.com>
2023-03-19 18:04:45 +0530

    Second Commit

--- Snake_Game/src/main/java/SnakeGame.java
+++ Snake_Game/src/main/java/SnakeGame.java
@@ -2,6 +2,9 @@
  * @Amresh Tripathy
  */
 
-public class SnakeGame {
-
+import javax.swing.*;
+public class SnakeGame extends JFrame{
+    public static void main(String[] args) {
+        System.out.println("Amresh");
+    }
 }
